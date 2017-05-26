dc081891172e9ae5cea8d76f0fa7e0b05fa83725
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-05-26 16:28:53 +0100

    Made server run

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(require('./controllers/index'));
 
 app.use(express.static('client/build'));
 
-app.listen(3000, function( {
+app.listen(3000, function(){
   console.log("App running on port " + this.address().port);
-}));
+});
