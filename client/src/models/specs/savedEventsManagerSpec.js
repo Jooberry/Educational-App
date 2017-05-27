c8131db9a63af1e4f62b5b14ac38787f93b62158
Paule AD <akramaite.paule@gmail.com>
2017-05-27 13:23:27 +0100

    written two tests for the checkIfNotOverlap function

--- client/src/models/specs/savedEventsManagerSpec.js
+++ client/src/models/specs/savedEventsManagerSpec.js
@@ -1,1 +1,39 @@
+var assert = require('assert')
 
+var EventManager = require("../EventManager")
+var Event = require("../Event")
+
+beforeEach(function () {
+  savedEvents = [];
+  event1 = new Event({  "performances": [{
+      "start": "2017-07-20 17:30:00",
+      "end": "2017-07-20 18:30:00",
+      "price": 15.5
+    }]})
+  event2 = new Event({  "performances": [{
+      "start": "2017-07-20 19:30:00",
+      "end": "2017-07-20 20:30:00",
+      "price": 15.5
+    }]})
+  event3 = new Event({  "performances": [{
+      "start": "2017-07-20 18:00:00",
+      "end": "2017-07-20 18:30:00",
+      "price": 15.5
+    }]})
+  event4 = new Event({  "performances": [{
+      "start": "2017-07-21 17:30:00",
+      "end": "2017-07-21 18:30:00",
+      "price": 15.5
+    }]})
+  savedEvents.add(event1, event2)
+  eventManager = new EventManager();
+});
+
+describe("EventManager", function(){
+  it("check function should return true if event is not overlapping", function(){
+    eventManager.checkEventsDoNotOverlap(savedEvents, event4)
+  })
+  it("check function should return false if the event is overlapping", function(){
+    eventManager.checkEventsDoNotOverlap(savedEvents, event3)
+  })
+})
