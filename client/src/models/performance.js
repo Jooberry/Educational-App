ffb5e7267f1407ffab3d22a94ef082bbff5b1ae7
Andrew Gurney <MrAJGurney@Gmail.com>
2017-05-27 19:32:35 +0100

    Updated performance comment style

--- client/src/models/performance.js
+++ client/src/models/performance.js
@@ -1,8 +1,10 @@
-// {
-//   "code": "55537b7f1d6a5131db0d794aa6a13174be9fc7d4-733900",
-//   "start": "2017-07-20 17:30:00",
-//   "end": "2017-07-20 18:30:00"
-// }
+/*
+ * {
+ *   "code": "55537b7f1d6a5131db0d794aa6a13174be9fc7d4-733900",
+ *   "start": "2017-07-20 17:30:00",
+ *   "end": "2017-07-20 18:30:00"
+ * }
+ */
 
 var Peformance = function(options) {
   this.code = options.code;
