c07459e3f5ec13b0cef2da806d7a9a630d14b2ad
Reece Baird <reeceybaird@hotmail.com>
2017-05-28 00:22:39 +0100

    drop marker added

--- client/build/public/mapWrapper.js
+++ client/build/public/mapWrapper.js
@@ -9,16 +9,19 @@ var MapWrapper = function(container, coords, zoom){
 MapWrapper.prototype = {
   addMarker: function(coords){
     var marker = new google.maps.Marker({
+      animation: google.maps.Animation.DROP,
       position: coords,
       map: this.googleMap
     });
   },
 
   addClickEvent: function(){
-    google.maps.event.addListener(this.googleMap, "click", function(event){
-      console.log("add Click Event Working");
-    });
+    google.maps.event.addListener(this.googleMap, 'click', function(event){
+      var position = { lat: event.latLng.lat(), lng: event.latLng.lng()}
+      this.addMarker(position);
+    }.bind(this));
+    }
   }
-}
+
 
 module.exports = MapWrapper;
