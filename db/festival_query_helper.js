5c23b23627af566d60db243788515817c44038dc
Andrew Gurney <MrAJGurney@Gmail.com>
2017-05-27 19:03:28 +0100

    Altered db to handle event + performance speperatly

--- db/festival_query_helper.js
+++ db/festival_query_helper.js
@@ -1,31 +1,65 @@
 var MongoClient = require('mongodb').MongoClient;
 
+/*
+ * events =>
+ *      "title": "Ingrid Lucia Sings Billie Holiday ",
+ *      "code": "55537b7f1d6a5131db0d794aa6a13174be9fc7d4-733900"
+ * performances =>
+ *      "start": "2017-07-20 17:30:00",
+ *      "end": "2017-07-20 18:30:00",
+ *      "code": "55537b7f1d6a5131db0d794aa6a13174be9fc7d4-733900"
+ */
+
 var FestivalQueryHelper = function() {
   this.url = 'mongodb://localhost:27017/festival';
-  this.performances = 'performances';
 }
 
 FestivalQueryHelper.prototype = {
-  allShows: function(onQueryFinished) {
+  allEvents: function(onQueryFinished) {
     MongoClient.connect(this.url, function(err, db) {
       if (err) {
-        throw "FAILED TO ACCESS |" + 'shows' + "| COLLECTION IN |" + this.url + "| DATABASE";
+        throw "FAILED TO ACCESS |" + 'events' + "| COLLECTION IN |" + this.url + "| DATABASE";
         return;
       }
-      var collection = db.collection('shows');
+      var collection = db.collection('events');
       collection.find().toArray(function(err, docs) {
         onQueryFinished(docs);
       });
     });
   },
-  addShow: function(showToAdd, onQueryFinished) {
+  addEvents: function(eventToAdd, onQueryFinished) {
     MongoClient.connect(this.url, function(err, db) {
       if (err) {
-        throw "FAILED TO ACCESS |" + 'shows' + "| COLLECTION IN |" + this.url + "| DATABASE";
+        throw "FAILED TO ACCESS |" + 'events' + "| COLLECTION IN |" + this.url + "| DATABASE";
         return;
       }
-      var collection = db.collection('shows');
-      collection.insert(showToAdd);
+      var collection = db.collection('events');
+      collection.insert(eventToAdd);
+      collection.find().toArray(function(err, docs) {
+        onQueryFinished(docs);
+      });
+    });
+  },
+  allPerformances: function(onQueryFinished) {
+    MongoClient.connect(this.url, function(err, db) {
+      if (err) {
+        throw "FAILED TO ACCESS |" + 'performances' + "| COLLECTION IN |" + this.url + "| DATABASE";
+        return;
+      }
+      var collection = db.collection('performances');
+      collection.find().toArray(function(err, docs) {
+        onQueryFinished(docs);
+      });
+    });
+  },
+  addPerformances: function(performanceToAdd, onQueryFinished) {
+    MongoClient.connect(this.url, function(err, db) {
+      if (err) {
+        throw "FAILED TO ACCESS |" + 'performancess' + "| COLLECTION IN |" + this.url + "| DATABASE";
+        return;
+      }
+      var collection = db.collection('performances');
+      collection.insert(performanceToAdd);
       collection.find().toArray(function(err, docs) {
         onQueryFinished(docs);
       });
