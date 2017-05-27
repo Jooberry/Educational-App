4a2f8532594eadf86457e8a21025f875bf020026
Andrew Gurney <MrAJGurney@Gmail.com>
2017-05-27 19:47:29 +0100

    Testing change in .gitignore

---
+++ client/src/helpers/remote_festival_api_helper.js
@@ -0,0 +1,12 @@
+var RemoteFestivalAPIHelper = function() {};
+
+RemoteFestivalAPIHelper.prototype = {
+  populateLocalDatabase: function() {
+    // Get all events
+    // Get all performances from events
+    // Store all events into local database
+    // Store all performances into local database
+  };
+};
+
+module.exports = RemoteFestivalAPIHelper;
