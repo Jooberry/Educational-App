c90a701a17c98f9ea6edf5c04b68c5f43b24b8f6
Paule AD <akramaite.paule@gmail.com>
2017-05-28 11:04:35 +0100

    query all function done, not tested

---
+++ controllers/saved_events_controller.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var savedEventsRouter = express.Router()
+
+var SavedEventQuery = require('../db/savedEventQuery')
+
+var query = new SavedEventQuery()
+
+savedEventsRouter.get('/', functioin(req, res){
+  query.all(function(events){
+    res.json(events)
+  })
+})
+
+SavedEventQuery.post("/", function(req, res){
+  
+})
