5df8a01f2e155b964db1a6b3ea06f5d471ced51e
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-05-28 04:36:34 +0100

    Made previous and next buttons work in terms of showing the next and previous items in the datesArray

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -6,6 +6,10 @@ var datesArray = [
   {
     date: '2017-07-24',
     title: 'Continuing to Jazz'
+  },
+  {
+    date: '2017-07-26',
+    title: 'Tired of Coding Jazz'
   }
 ]
 
@@ -21,21 +25,51 @@ var UI = function() {
 
   this.onPreviousClick();
   this.onNextClick();
+
+  counter = 0;
 };
 
 UI.prototype = {
 
   onPreviousClick: function() {
+
     var previousButton = document.getElementById('previous');
     previousButton.addEventListener('click', function() {
     
+      var infoText = document.getElementById('info-text');
+      infoText.innerHTML = '';
+
+      counter --;
+      
+      var eventTitle = document.createElement('h1');
+      eventTitle.innerText = datesArray[counter-1].title;
+      infoText.appendChild(eventTitle);
+
+      var eventDate = document.createElement('p');
+      eventDate.innerText = datesArray[counter-1].date;
+      infoText.appendChild(eventDate);
+
     });
   },
 
   onNextClick: function() {
+
     var nextButton = document.getElementById('next');
     nextButton.addEventListener('click', function() {
-      
+
+      var infoText = document.getElementById('info-text');
+      infoText.innerHTML = '';
+
+      counter ++;
+
+      var eventTitle = document.createElement('h1');
+      eventTitle.innerText = datesArray[counter-1].title;
+      infoText.appendChild(eventTitle);
+
+      var eventDate = document.createElement('p');
+      eventDate.innerText = datesArray[counter-1].date;
+      infoText.appendChild(eventDate);
+
     });
   }
 
